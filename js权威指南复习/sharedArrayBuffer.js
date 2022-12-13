704c002a870b56c7b59f0aab3512767b8a0ac2cf
蔡豪 <1101767893@qq.com>
2022-12-13 21:04:18 +0800

    SharedArrayBufferApi (web端已禁用)

--- js权威指南复习/sharedArrayBuffer.js
+++ js权威指南复习/sharedArrayBuffer.js
@@ -1,7 +1,37 @@
 const workerScript =
-self.onmessage = ({data})=>{
+    `
+self.onmessage =
+({data})=>{
     const view = new Uint32Array(data)
     for (let i = 0;i<1E6;i++){
         view[0]+=1 // v
     }
+    
+    self.postMessage(null)
+}
+`
+const workerScriptBlobUrl = URL.createObjectURL(new Blob([workerScript]))
+
+const  workers = []
+for (let i = 0;i< 4 ;i++){
+    workers.push(new Worker(workerScriptBlobUrl))
+}
+
+let responseCount = 0
+
+for (const worker of workers ){
+    worker.onmessage =()=>{
+        if(++responseCount === worker.length){
+            console.log(`final counte ${view[0]}`)
+        }
+
+    }
+}
+const sharedArrayBuffer = new SharedArrayBuffer(4)   // 注意 浏览器中因为安全原因已经禁用了SharedArrayBuffer对象
+
+const view = new Uint32Array(sharedArrayBuffer)
+view[0] = 1
+
+for(const worker of workers){
+    worker.postMessage(sharedArrayBuffer)
 }
